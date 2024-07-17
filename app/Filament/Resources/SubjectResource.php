095a87719df2b28833ed2886fa58cc676ce90c73
Mansoor Khan <8mansoorkhan@gmail.com>
2024-07-17 16:17:03 +0500

    add resource for subject

---
+++ app/Filament/Resources/SubjectResource.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SubjectResource\Pages;
+use App\Models\Subject;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class SubjectResource extends Resource
+{
+    protected static ?string $model = Subject::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('user_id')
+                    ->label('Teacher')
+                    ->relationship('user', 'name')
+                    ->required(),
+                Forms\Components\Select::make('education_level_id')
+                    ->relationship('educationLevel', 'name')
+                    ->required(),
+                Forms\Components\TextInput::make('name')
+                    ->required(),
+                Forms\Components\TextInput::make('max_marks')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('min_passing_marks')
+                    ->required()
+                    ->numeric(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('user.name')
+                    ->label('Teacher')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('educationLevel.name')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('max_marks')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('min_passing_marks')
+                    ->numeric()
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSubjects::route('/'),
+            'create' => Pages\CreateSubject::route('/create'),
+            'edit' => Pages\EditSubject::route('/{record}/edit'),
+        ];
+    }
+}
